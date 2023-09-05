0d4a31276e21c4e39c4873e9d46a7077de33d19b
patriciohuang <patriciohuang20@gmail.com>
2023-09-05 11:26:54 +0200

    Basic setups

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+function Header() {
+  return (
+    <header>
+      <img src="/img/Logo.svg" alt="Logo lemon"/>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
